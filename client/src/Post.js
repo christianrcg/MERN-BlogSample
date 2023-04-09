87d0f88b38a345d644313cb1296cb0dbdde84934
christianrcg <gabrielarcega01@gmail.com>
2023-04-09 13:22:17 +0800

    Fix Commit, Nested Repo

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+          <img src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617559" alt="" />
+        </div>
+        <div className="texts">
+          <h2> Iphone 14 pro max</h2>
+          <p className="info">
+            <a href="" className="author">Steve Jobs</a>
+            <time>2023-04-06 14:20</time>
+          </p>
+          <p className="summary">iPhone 14 Pro and 14 Pro Max. With Dynamic Island. Crash Detection. An innovative 48MP camera. And A16 Bionic.</p>
+        </div>
+      </div>
+    );
+}
