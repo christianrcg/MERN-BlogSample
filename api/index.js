d7125495b97e69ead4cba52584d467ee3735ace7
christianrcg <gabrielarcega01@gmail.com>
2023-04-09 22:36:41 +0800

    Second Commit: Registers

--- api/index.js
+++ api/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
 const app = express();
 
-app.post('/registert', (req,res) =>{
-    res.json('test ok - 3');
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://iblog:NeiCmE4zcQH48Ayf@cluster0.mdggqin.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async (req,res) =>{
+    const {username,password} = req.body;
+    try{
+        const userDoc = await User.create({username,password,});
+        res.json(userDoc);
+    } catch(e){
+        res.status(400).json(e);
+    }
 } );
 
 app.listen(4000);
+
+
+//iblog                   username
+//NeiCmE4zcQH48Ayf        password 
+
+//connection string to mongodb
+//mongodb+srv://iblog:<password>@cluster0.mdggqin.mongodb.net/?retryWrites=true&w=majority  
+
+//modified connection string
+// mongodb+srv://iblog:NeiCmE4zcQH48Ayf@cluster0.mdggqin.mongodb.net/?retryWrites=true&w=majority
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -3,14 +3,21 @@ import { useState } from "react";
 export default function Registerpage(){
     const[username, setUsername] = useState('');
     const[password, setPassword] = useState('');
+
     async function register(ev){
         ev.preventDefault();
-        await fetch('http://localhost:4000',{
+        const response = await fetch('http://localhost:4000/register', {
             method: 'POST',
             body: JSON.stringify({username,password}),
-            headers: {'Content-Type':'application/json'}
-        })
+            headers: {'Content-Type':'application/json'},
+        });
+        if(response.status === 200){
+            alert('Registration Successful');
+        } else{
+            alert('Registration Failed');
+        } 
     }
+
     return(
         <form className="register" onSubmit={register}>
             <h1>Register</h1>
