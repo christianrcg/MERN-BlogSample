577a49cb3433f7931c342dc1bf3290ab05beca41
christianrcg <gabrielarcega01@gmail.com>
2023-04-09 23:34:12 +0800

    2.1, Registers With Hashed password

--- api/index.js
+++ api/index.js
@@ -2,23 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
 
+//hashing password
+const salt = bcrypt.genSaltSync(10);
+
+//uses to express api
 app.use(cors());
 app.use(express.json());
 
+//connect to mongodb
 mongoose.connect('mongodb+srv://iblog:NeiCmE4zcQH48Ayf@cluster0.mdggqin.mongodb.net/?retryWrites=true&w=majority');
 
+//registers user, have error handling if a username and password is already registered
 app.post('/register', async (req,res) =>{
     const {username,password} = req.body;
     try{
-        const userDoc = await User.create({username,password,});
+        const userDoc = await User.create({
+            username,
+            password:bcrypt.hashSync(password, salt),
+        });
         res.json(userDoc);
     } catch(e){
         res.status(400).json(e);
     }
 } );
 
+//sets the app to listen to 4000 server
 app.listen(4000);
 
 
